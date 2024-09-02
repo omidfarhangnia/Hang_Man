895d6750a2babc256315ed1589c56c2ca2233a13
omidfarhangnia <83111084+omidfarhangnia@users.noreply.github.com>
2024-09-02 12:06:39 +0330

    adding data

---
+++ script/data.js
@@ -0,0 +1,74 @@
+export default {
+  "animalsName": [
+    "donkey",
+    "cat",
+    "chicken",
+    "chinchilla",
+    "crocodile",
+    "dog",
+    "dolphin",
+    "tiger",
+    "turtle",
+    "zebra"
+  ],
+  "sportsName": [
+    "parkour",
+    "diving",
+    "archery",
+    "baseball",
+    "basketball",
+    "ringball",
+    "hockey",
+    "polo",
+    "badminton",
+    "football"
+  ],
+  "clothesName": [
+    "hat",
+    "shoes",
+    "top",
+    "robe",
+    "sandals",
+    "poncho",
+    "slippers",
+    "overalls",
+    "jeans",
+    "coat"
+  ],
+  "fruitsName": [
+    "apple",
+    "banana",
+    "pineapple",
+    "strawberry",
+    "blueberry",
+    "kiwi",
+    "cherry",
+    "orange",
+    "watermelon",
+    "avocado"
+  ],
+  "foodsName": [
+    "cookie",
+    "toast",
+    "cheesecake",
+    "icecream",
+    "fish",
+    "omelette",
+    "milk",
+    "pizza",
+    "sandwich",
+    "hamburger"
+  ],
+  "colorsName": [
+    "pink",
+    "orange",
+    "purple",
+    "scarlet",
+    "green",
+    "yellow",
+    "red",
+    "bone",
+    "bronze",
+    "brown"
+  ]
+}
